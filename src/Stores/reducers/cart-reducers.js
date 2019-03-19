3bf61ba5c266836e0bf7ecb264236798617bc1fb
Jamshith P <jammuavu@gmail.com>
2019-03-19 10:15:12 +0530

    updated reducers
    Signed-off-by: Jamshith P <jammuavu@gmail.com>

--- src/Stores/reducers/cart-reducers.js
+++ src/Stores/reducers/cart-reducers.js
@@ -26,14 +26,22 @@ const addToCart = (state, action)=> {
     let addedItem = state.items.find(item=> item.id === action.id)
     //check if the action id exists in the addedItems
    let existed_item= state.addedItems.find(item=> action.id === item.id)
-   if(existed_item)
-   {
-      addedItem.quantity += 1 
-       return{
-          ...state,
-           total: state.total + addedItem.price 
-            }
+   if(existed_item){
+        addedItem.quantity += 1 
+        return{...state,total: state.total + addedItem.price}
+    }
+    else{
+        addedItem.quantity = 1;
+        //calculating the total
+        let newTotal = state.total + addedItem.price 
+        
+        return{
+            ...state,
+            addedItems: [...state.addedItems, addedItem],
+            total : newTotal
         }
+        
+    }
 }
 
 const cartReducer = ( state = initialState, action ) => {
